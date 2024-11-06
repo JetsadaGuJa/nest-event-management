aab6dc3b25c57572af6eea4a529183d7a6215737
Jetsada Gujan <jamanggaza@gmail.com>
2024-11-06 10:30:24 +0700

    update validate error

--- src/main.ts
+++ src/main.ts
@@ -7,20 +7,54 @@ import { AppModule } from './app.module';
 // import { EmojiLogger } from './logger';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { ResponseInterceptor } from './response.interceptor';
+import { ValidationError } from 'class-validator';
 // import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-client-exception.filter';
+
+function getAllConstraints(
+    errors: ValidationError[],
+    indexList?: string,
+): any[] {
+    const constraints: any[] = [];
+
+    errors.map((error, index) => {
+        {
+            if (error.constraints) {
+                const constraintValues = Object.values(error.constraints);
+                constraints.push(
+                    `${indexList ? `index: ${indexList} ` : ''}property: "${
+                        error.property
+                    }" message: ${constraintValues}`,
+                );
+            }
+
+            if (error.children) {
+                const childConstraints = getAllConstraints(
+                    error.children,
+                    `${index}`,
+                );
+                constraints.push(...childConstraints);
+            }
+        }
+    });
+
+    return constraints;
+}
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {});
-
     app.useGlobalPipes(
         new ValidationPipe({
             exceptionFactory: (errors) => {
+                // console.log(
+                //     'errorserrorserrorserrorserrors =>',
+                //     JSON.stringify(errors),
+                // );
                 // const result = errors.map((error: any) => ({
                 //     property: error.property,
                 //     message:
                 //         error.constraints[Object.keys(error.constraints)[0]],
                 // }));
-                return new BadRequestException(errors);
+                return new BadRequestException(getAllConstraints(errors));
             },
             stopAtFirstError: true,
         }),
